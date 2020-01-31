e495823bff6b239a53bd3c631a303ba7933364e7
Choengchai Phachonyut <choengchaip@gmail.com>
2020-01-31 22:40:12 +0700

    31012020

--- lib/Services/Authentication.dart
+++ lib/Services/Authentication.dart
@@ -10,8 +10,8 @@ class Authentication extends ChangeNotifier{
   double longitude;
   bool isLogin;
   final PROTOCAL = 'http';
-  final IP = '192.168.1.9';
-  final PORT = 8080;
+  final IP = 'chengchai.000webhostapp.com';
+  final PORT = 80;
 
   get GETPROTOCAL => this.PROTOCAL;
   get GETIP => this.IP;
